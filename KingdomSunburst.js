8eda95e04bffd8c1a81817f758b7bb90f468cfc3
zyibj <zyibj@cn.ibm.com>
2019-01-15 14:05:11 +0800

    updated d3 js

--- KingdomSunburst.js
+++ KingdomSunburst.js
@@ -1,4 +1,4 @@
-define(["http://d3js.org/d3.v5.min.js"], function(d3) {
+define(["https://ajax.googleapis.com/ajax/libs/d3js/5.7.0/d3.min.js"], function(d3) {
   "use strict";
 
   function KingdomSunburst() {}
