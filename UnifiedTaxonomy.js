528777566d9b9c679b7e25a7308c695f0e8a08d0
zyibj <zyibj@cn.ibm.com>
2019-03-18 11:34:01 +0800

    Updated UT d3 source

--- UnifiedTaxonomy.js
+++ UnifiedTaxonomy.js
@@ -1,4 +1,4 @@
-define( ["http://d3js.org/d3.v5.min.js"], function( d3 ) {
+define( ["https://ajax.googleapis.com/ajax/libs/d3js/5.7.0/d3.min.js"], function( d3 ) {
 "use strict";
 
 function UTRadialTree()
@@ -9,17 +9,17 @@ UTRadialTree.prototype.draw = function( oControlHost )
 {
 	var width=1640;
     var	height=1640;
-	
+
 	var svg = d3.select( oControlHost.container )
 	  .append("svg")
 		.attr("width", width)
 		.attr("height", height);
 	var g = svg.append("g").attr("transform", "translate(" + (width / 2 + 0) + "," + (height / 2 + 0) + ")");
-	
+
 	var tree = d3.tree()
 		.size([2 * Math.PI, 800])
 		.separation((a, b) => (a.parent == b.parent ? 1 : 2) / a.depth);
-		
+
 	var nestedData = d3.nest()
 		.key(d => 'Unified Taxonomy')
 		.key(d => d.L10)
@@ -61,12 +61,12 @@ UTRadialTree.prototype.draw = function( oControlHost )
 	  .attr("text-anchor", d => d.x < Math.PI === !d.children ? "start" : "end")
 	  .attr("transform", d => "rotate(" + (d.x < Math.PI ? d.x - Math.PI / 2 : d.x + Math.PI / 2) * 180 / Math.PI + ")")
 	  .text(d => (d.height == 0 ? '':d.data.key));
-	  
+
 	function radialPoint(x, y) {
 	  return [(y = +y) * Math.cos(x -= Math.PI / 2), y * Math.sin(x)];
-	  
+
 	}
-	
+
 };
 
 UTRadialTree.prototype.setData = function( oControlHost, oDataStore )
@@ -77,11 +77,11 @@ UTRadialTree.prototype.setData = function( oControlHost, oDataStore )
 	var iRowCount = oDataStore.rowCount;
 	var jColCount = oDataStore.columnCount;
 	var colNames = oDataStore.columnNames;
-	for ( var iRow = 0; iRow < iRowCount; iRow++ ){		
+	for ( var iRow = 0; iRow < iRowCount; iRow++ ){
 		let newObj = {};
 		for ( var jCol = 0; jCol < jColCount; jCol++ )
 			newObj[colNames[jCol]] = oDataStore.getCellValue(iRow,jCol);
-		this.m_aData.push(newObj);		
+		this.m_aData.push(newObj);
 	}
 
 };
